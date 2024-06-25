4942d0992e7cbbdcefd6e2c082784e0571dbacbd
Iker <ikersampedrosanchez@gmail.com>
2024-06-25 08:58:47 +0200

    reload

--- planner/planner/collision_avoidance.py
+++ planner/planner/collision_avoidance.py
@@ -14,6 +14,7 @@ class Collision_avoidance(Node):
         self.positions = [0]
         self.initial_positions = [0]  #It will be used to define the drone trajectories
         self.publishers_ = [0]
+        self.next_waypoints = [0]
         self.prev_result = []
         self.countDiffDrones = [0]  #Controls if the position of all drones have arrived
         self.prevCount = 0
@@ -22,26 +23,30 @@ class Collision_avoidance(Node):
         self.speeds_subscription = self.create_subscription(DoubleArray, f'/speeds', self.speeds_callback, 10)
 
     def drone_ids_callback(self, msg):
-        drone_ids = msg.drone_ids
-        self.positions = self.positions * len(drone_ids)
-        self.initial_positions = self.initial_positions * len(drone_ids)
-        self.publishers_ = self.publishers_ * len(drone_ids)
-        self.countDiffDrones = self.countDiffDrones * len(drone_ids)
-        for drone_id in drone_ids:
-            Pose_subscription(drone_id, len(drone_ids), self)  #It creates one subscription for the pos of each drone
+        self.drone_ids = msg.drone_ids
+        self.positions = self.positions * len(self.drone_ids)
+        self.initial_positions = self.initial_positions * len(self.drone_ids)
+        self.publishers_ = self.publishers_ * len(self.drone_ids)
+        self.countDiffDrones = self.countDiffDrones * len(self.drone_ids)
+        self.next_waypoints = self.next_waypoints * len(self.drone_ids)
+        for _ in self.drone_ids:
+            Pose_subscription(index, len(self.drone_ids), self)  #It creates one subscription for the pos of each drone
+            index += 1
     
     def speeds_callback(self, msg):
         self.speeds = msg.speeds
 
 class Pose_subscription(Node):
-    def __init__(self, drone_id, nDrones, collision_avoidance):
+    def __init__(self, index, nDrones, collision_avoidance):
+        self.collision_avoidance = collision_avoidance
+        drone_id = self.collision_avoidance.drone_ids[index]
         super().__init__('Collision_avoidance' + drone_id)
         self.count = 0  #Counts the amount of positions of the same drone
-        self.index = int(drone_id.replace("drone_", ""))
+        self.index = index
         self.collision_avoidance = collision_avoidance
         self.nDrones = nDrones
-
         self.collision_avoidance.publishers_[self.index] = self.create_publisher(IntArray, f'/{drone_id}/collisions', 10)
+        self.wps_subscription = self.collision_avoidance.create_subscription(Waypoints, f'/{drone_id}/route', self.waypoints_callback, 10)
         self.pose_subscription = self.collision_avoidance.create_subscription(PoseStamped, f'/{drone_id}/pose', self.new_pose_callback, 10)
         
         
@@ -54,7 +59,11 @@ class Pose_subscription(Node):
             if self.collision_avoidance.countDiffDrones == ([1] * self.nDrones):
                 
                 self.collision_avoidance.countDiffDrones = ([0] * self.nDrones)
-                hits_tool = Hits_toolsv3(self.collision_avoidance.initial_positions, self.collision_avoidance.positions, self.collision_avoidance.speeds, [1])
+
+                for i in range(self.nDrones):
+                    self.collision_avoidance.next_waypoints[i] = self.next_waypoint(self.collision_avoidance.waypoints[i], self.collision_avoidance.positions[i])
+                
+                hits_tool = Hits_toolsv3(self.collision_avoidance.initial_positions, self.collision_avoidance.positions, self.collision_avoidance.speeds, self.collision_avoidance.next_waypoints)
                 result = hits_tool.nearest_positions()
 
                 #If the difference between count and prevCount equals to 30 it means that about 3
@@ -70,6 +79,31 @@ class Pose_subscription(Node):
         elif self.count % 5 == 1:
             self.collision_avoidance.initial_positions[self.index] = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
 
+    def waypoints_callback(self, msg):
+        self.collision_avoidance.waypoints[self.index] = np.array(msg.wps, dtype=np.float64).reshape((int)(len(msg.wps)/3), 3)
+    
+    def next_waypoint(self, waypoints, position):
+        finded = True
+        i = 0
+        j = 1
+        while not finded:
+                finded = self.same_line(waypoints[i], position, waypoints[j])
+                i += 1
+                j += 1
+        return waypoints[j]
+        
+
+    def same_line(point0, point1, point2) -> bool:
+        vector1 = (point1[0] - point0[0], point1[1] - point0[1], point1[2] - point0[2])
+        vector2 = (point2[0] - point0[0], point2[1] - point0[1], point2[2] - point0[2])
+
+        diff_x = vector1[1] * vector2[2] - vector1[2] * vector2[1]
+        diff_y = vector1[2] * vector2[0] - vector1[0] * vector2[2]
+        diff_z = vector1[0] * vector2[1] - vector1[1] * vector2[0]
+
+        if abs(diff_x) < 0.001 and abs(diff_y) < 0.001 and abs(diff_z) < 0.001:
+            return True
+        return False
 def main():
     rclpy.init()
 
--- planner/planner/hits_toolsv3.py
+++ planner/planner/hits_toolsv3.py
@@ -3,15 +3,12 @@
 import numpy as np
 
 class Hits_toolsv3():
-    def __init__(self, initial_positions, final_positions, speeds, sizes):
+    def __init__(self, initial_positions, final_positions, speeds, next_wp):
         # final_positions contains tuples that contain the initial and the final position defining the trajectory of each drone
         self.final_positions = final_positions
         self.initial_positions = initial_positions
         self.speeds = speeds
-        self.sizeDrone = sizes[0]
-        for sizeD in sizes:
-            if (sizeD > self.sizeDrone):
-                self.sizeDrone = sizeD
+        self.next_wp = next_wp
             
     def nearest_positions(self):
         result = set()  # Use a set to avoid duplicates
@@ -22,20 +19,20 @@ class Hits_toolsv3():
                     p1_final =  np.array(self.final_positions[i])
                     p2_final =  np.array(self.final_positions[j])
                     distance = np.linalg.norm(p1_final - p2_final)
-                    if distance < 20 * self.sizeDrone:
+                    if distance < 30:
                         p1_initial =  np.array(self.initial_positions[i])
                         p2_initial =  np.array(self.initial_positions[j])
                         initial_distance = np.linalg.norm(p1_initial - p2_initial)
                         if (initial_distance >= distance):  #They are getting closer
                             intersection = self.trajectory_intersection(p1_initial, p1_final, p2_initial, p2_final)
                             intersection = [intersection[0], intersection[1], p1_final[2]]
-                            d1 = np.linalg.norm(intersection - p1_final)
-                            d2 = np.linalg.norm(intersection - p2_final)
-                            t1 = d1 / self.speeds[i]
-                            t2 = d2 / self.speeds[j]
-                            print(t2)
-                            print(t1)
-                            if ((d1 < 10 * self.sizeDrone or d2 < 10 * self.sizeDrone) and abs(t2 - t1) <= 2):
+                            d1_to_wps = np.linalg.norm(self.next_wp[i] - p1_final)
+                            d2_to_wps = np.linalg.norm(self.next_wp[j] - p2_final)
+                            d1_to_int = np.linalg.norm(p1_final - intersection)
+                            d2_to_int = np.linalg.norm(p2_final - intersection)
+                            t1 = d1_to_int / self.speeds[i]
+                            t2 = d2_to_int / self.speeds[j]
+                            if (d1_to_wps - d1_to_int > 2 and  d2_to_wps - d2_to_int > 2 and abs(t2 - t1) <= 2):
                                 result.add(i)
                                 result.add(j)
 
